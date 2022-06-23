ec0f287bc84e0af25ea4b6a69f3ba3e060cea13f
locavallero <lorenzo1.cavallero@mail.polimi.it>
2022-06-23 17:20:12 +0200

    Removed jobs

--- src/com/xilinx/rapidwright/examples/EntreeFloorplanner.java
+++ src/com/xilinx/rapidwright/examples/EntreeFloorplanner.java
@@ -1,19 +1,16 @@
 package com.xilinx.rapidwright.examples;
 
 import com.xilinx.rapidwright.design.Design;
-import com.xilinx.rapidwright.design.blocks.PBlock;
 import com.xilinx.rapidwright.design.blocks.PBlockGenerator;
-import com.xilinx.rapidwright.device.Device;
-import com.xilinx.rapidwright.util.FileTools;
-import com.xilinx.rapidwright.util.Job;
 import com.xilinx.rapidwright.util.JobQueue;
 import it.necst.entree.Tree;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -24,67 +21,12 @@ public class EntreeFloorplanner {
     public static int xb;
     public static int ya;
     public static int yb;
-    public static final String DEVICE = "xczu3eg-sbva484-1-i";
     static final int OVERHEAD_RATIO = 1;
     static final int COL_SIZE = 1;
     static final int ROW_SIZE = 60;
     static final int GROUP_NUMBER = 3; //Apriori defined number of groups
     static final String SHAPES_REPORT_FILE_NAME = "shape.txt";
-    static final String VIVADO_PATH = "/home/locav/Xilinx/Vivado/2021.2/bin/vivado";
-    static final String WORKING_DIR = "/home/locav/dcp/";//TODO: tmp director
-    private static void createTclScript(String scriptName, String dcpFileName, PBlock pblock){
-        String pblockName = dcpFileName.substring(102,135);
-        PrintWriter pw = null;
-        try {
-            pw = new PrintWriter(WORKING_DIR + scriptName);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException("ERROR: Couldn't create Tcl script " + scriptName);
-        }
-        pw.println("open_checkpoint " + dcpFileName);
-        pw.println("create_pblock " + pblockName);
-        pw.println("resize_pblock "+pblockName+" -add {"+pblock.toString()+"}");
-        pw.println("write_xdc -force " + WORKING_DIR + pblockName + ".xdc"); //mi basta creare un file xdc per ogni albero
-        pw.close();
-    }
-    private static void linkDesignTclScript(String scriptName, String dcpFileName_1, String xdcFileName){
-        PrintWriter pw = null;
-        try {
-            pw = new PrintWriter(WORKING_DIR + scriptName);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException("ERROR: Couldn't create Tcl script " + scriptName);
-        }
-        pw.println("open_checkpoint " + dcpFileName_1);
-        pw.println("read_xdc -no_add " + WORKING_DIR + xdcFileName + " -quiet_diff_pairs");
-        pw.println("write_checkpoint -force " + WORKING_DIR + dcpFileName_1);
-        pw.close();
-    }
-    public static Job createDCP(String dcpFileName, String s, String path){
-        String tclFileName = "run.tcl";
-        PBlock pblock = new PBlock(Device.getDevice(DEVICE), s);
-
-        Job j = JobQueue.createJob();
-        j.setRunDir(path);
-        j.setCommand(VIVADO_PATH + " -mode batch -source " + WORKING_DIR + tclFileName);
-        FileTools.makeDirs(WORKING_DIR);
-        createTclScript(tclFileName, dcpFileName, pblock);
-
-        return j;
-    }
-    public static Job createUnifiedDesign(String dcpFileName_1, String xdcFileName, String path){
-        String tclFileName = "link.tcl";
-
-        Job j = JobQueue.createJob();
-        j.setRunDir(path);
-        j.setCommand(VIVADO_PATH + " -mode batch -source " + WORKING_DIR + tclFileName);
-        FileTools.makeDirs(WORKING_DIR);
-        linkDesignTclScript(tclFileName, dcpFileName_1, xdcFileName);
-
-        return j;
-    }
     public static void main(String[] args) {
-        JobQueue jobs = new JobQueue();
 
         int TREE_NUMBER = args.length;
 
@@ -146,7 +88,7 @@ public class EntreeFloorplanner {
             String treeName = t.utilReport.substring(102, 135);
             String treeUtilReport = t.getUtilReport();
 
-            System.out.println(t);
+            //System.out.println(t);
             PBlockGenerator p = new PBlockGenerator.Builder()
                     .setGLOBAL_PBLOCK("/home/locav/global.txt") //why it doesn't avoid the pblock??!!
                     .setIP_NR_INSTANCES(1)
@@ -170,30 +112,17 @@ public class EntreeFloorplanner {
                 t.sliceCount = (yb - ya + 1) * (xb - xa + 1);
 
                 if(alreadySeen.contains(s)) continue;
-                System.out.println(treeName + "\t" + s + "\t" + t.sliceCount);//current state of trees
+                //System.out.println(treeName + "\t" + s + "\t" + t.sliceCount);//current state of trees
                 alreadySeen.add(s);
-                //TODO: assign coordinates
+
                 for (int j = i - TREE_NUMBER/GROUP_NUMBER + 1; j <= i; j++){
                     trees.get(j).setCoordinates(s);
                 }
 
-
-
-                Job j = JobQueue.createJob();
-                Job job = createDCP(treeUtilReport.replace("_utilization_synth.rpt", ".dcp"), s, treeUtilReport.substring(0, 59));//TODO: aggiusta gli argomenti
-                jobs.addJob(job);
-
                 requested--;
                 if(requested == 0) break;
             }
-
-
-
-            //TODO: link checkpoint: checkpoint_1 = checkpoint_1 + treeName.xdc
-            Job j = createUnifiedDesign("checkpoint_1.dcp", treeName + ".xdc", WORKING_DIR);
-            jobs.addJob(j);
         }
-        //jobs.runAllToCompletion();//TODO: attenzione all'ordine di esecuzione degli script! non usare JobQueue ma usa job.launchJob()
 
         for (Tree t : trees){
             System.out.println(t);
